151bb07b5422c424612b6bdeb492ddf258b1cb21
aborruso <aborruso@gmail.com>
2019-01-17 22:40:05 +0100

    primo upload

---
+++ datiConfiscatiBene.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+### OpenCoesione ###
+dirOC="$folder/pubblicaAmministrazione/OpenCoesione"
+mkdir -p "$dirOC"
+
+rm "$dirOC"/opencoesione.zip
+rm "$dirOC"/progetti.csv
+
+# scarica i dati
+
+curl -L "https://opencoesione.gov.it/it/progetti/csv/?q=&selected_facets=focus:beni_confiscati&selected_facets=is_pubblicato:true" >"$dirOC"/opencoesione.zip
+unzip "$dirOC"/opencoesione.zip -d "$dirOC"
+# scarica i metadati
+curl -L "https://opencoesione.gov.it/media/opendata/metadati_progetti_tracciato_esteso.xls" >"$dirOC"/metadati_progetti_tracciato_esteso.xls
+
+mlr -I --csv --ifs ";" --ofs "," clean-whitespace "$dirOC"/progetti.csv 
